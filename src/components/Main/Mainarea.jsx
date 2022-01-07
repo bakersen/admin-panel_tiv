2531a5725af3815cdf4b6bedc932a71db2004b48
michael <mikeamone@gmail.com>
2022-01-07 00:55:16 -0800

    First commit

---
+++ src/components/Main/Mainarea.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './mainarea.css';
+// import Members from '../members/Members'
+import Users from '../users/Users'
+
+export default function Main() {
+
+   return (
+        <div className="main-area ">
+            <Users />
+        </div>
+    )
+}
