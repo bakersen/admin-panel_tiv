202109130717c5a47539d8d578b7bbcbb0a755e5
Baker Sentamu <bsentamu@innovationvillage.co.ug>
2022-01-28 14:41:39 +0300

    Reload implemented on all pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Topbar from './components/topbar/Topbar'
+import Topbar from './components/Topbar/Topbar'
 import Sidebar from './components/Sidebar/Sidebar';
 import { Container, Row } from 'react-bootstrap'
 import {createTheme, ThemeProvider} from '@material-ui/core'
@@ -9,7 +9,7 @@ import  Members from './pages/Members';
 import  Jobs from './pages/Jobs';
 import  Startups from './pages/Startups';
 import './App.css';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import { BrowserRouter,Routes, Route, Navigate} from 'react-router-dom';
 
 const theme = createTheme({
   palette:{
@@ -53,6 +53,12 @@ function App() {
               <Route path="startups" element={<Startups />} />
               <Route path="jobs" element={<Jobs />} />
               <Route path="events" element={<Events />} />
+              <Route exact path="/reload" element={<Navigate to="/"/>} />
+              <Route exact path="members/reload" element={<Navigate to="/"/>} />
+              <Route exact path="startups/reload" element={<Navigate to="/"/>} />
+              <Route exact path="posts/reload" element={<Navigate to="/"/>} />
+              <Route exact path="jobs/reload" element={<Navigate to="/"/>} />
+              <Route exact path="events/reload" element={<Navigate to="/events"/>} />
             </Routes>
           </div>
         </Row>
